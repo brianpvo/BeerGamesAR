d34fd8600e411bdf565e66608e21a3423e062741
rayjialin <ruijialin@gmail.com>
2018-06-03 14:07:21 -0700

    initial commit for creating player state

---
+++ BeerGamesAR/GameViewController+PlayerState.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewController+PlayerState.swift
+//  BeerGamesAR
+//
+//  Created by ruijia lin on 6/3/18.
+//  Copyright © 2018 Brian Vo & Ray Lin & Ramen Singh & Tyler Boudreau. All rights reserved.
+//
+
+import UIKit
