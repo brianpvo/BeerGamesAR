206df5c5f6b54fbfe2357a7d1f41d21009c8d31e
rayjialin <ruijialin@gmail.com>
2018-06-10 17:30:02 -0700

    updated tracking status appearance

---
+++ BeerGamesAR/GameViewController+StatusBar.swift
@@ -0,0 +1,9 @@
+//
+//  GameViewController+StatusBar.swift
+//  BeerGamesAR
+//
+//  Created by ruijia lin on 6/9/18.
+//  Copyright © 2018 Brian Vo & Ray Lin & Ramen Singh & Tyler Boudreau. All rights reserved.
+//
+
+import Foundation
